3ba6268be60450038db41e79420809e03bb5dae0
Serghei Filimonov <filimonov.sergey@gmail.com>
2024-10-24 10:47:53 +0300

    Add p3.

--- queries.sql
+++ queries.sql
@@ -68,3 +68,33 @@ INSERT INTO borrowed_books (book_id, user_id, borrow_date, return_date)
 VALUES (1, 1, '2024-01-15', '2024-02-15'),
        (2, 2, '2024-01-20', NULL);
 
+-- p3 -- ********************************************************************
+SELECT 
+    orders.id AS order_id,
+    orders.date AS order_date,
+    products.name AS product_name,
+    categories.name AS category_name,
+    order_details.quantity,
+    customers.name AS customer_name,
+    customers.contact AS customer_contact,
+    customers.address AS customer_address,
+    customers.city AS customer_city,
+    customers.postal_code AS customer_postal_code,
+    customers.country AS customer_country,
+    employees.first_name AS employee_first_name,
+    employees.last_name AS employee_last_name,
+    employees.birthdate AS employee_birthdate,
+    employees.photo AS employee_photo,
+    employees.notes As employee_notes,
+    suppliers.name AS supplier_name,
+    shippers.name AS shipper_name,
+    shippers.phone AS shipper_phone
+FROM 
+    orders
+INNER JOIN customers ON orders.customer_id = customers.id
+INNER JOIN employees ON orders.employee_id = employees.employee_id
+INNER JOIN order_details ON orders.id = order_details.order_id
+INNER JOIN products ON order_details.product_id = products.id
+INNER JOIN categories ON products.category_id = categories.id
+INNER JOIN suppliers ON products.supplier_id = suppliers.id
+INNER JOIN shippers ON orders.shipper_id = shippers.id;
